55809dc507a1e8a89b752b92ff74efeaf94bbed8
tulloh-market <lailurrizki87@gmail.com>
2026-02-26 23:31:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    alert("Terima kasih sudah mengunjungi website ini!");
+}
+
+function submitForm() {
+    alert("Pesan berhasil dikirim!");
+    return false; // supaya tidak reload
+}
